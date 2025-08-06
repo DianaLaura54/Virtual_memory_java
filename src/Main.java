718c58c7c7d6be8eb0ffc74e94d7cf7c3f84caaf
DianaLaura54 <101946736+DianaLaura54@users.noreply.github.com>
2025-08-06 23:07:39 +0300

    m
    m

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import view.UserView;
 
 public class Main {
     public static void main(String[] args) {
-      
+
         UserView userView = new UserView();
         SelectionController controller = new SelectionController(userView);
     }
